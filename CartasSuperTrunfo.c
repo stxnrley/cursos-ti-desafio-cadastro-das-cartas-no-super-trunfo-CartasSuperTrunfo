5abedc5717003f1e6dd6ee9726e4d0784de6ea5c
stxnrley <stanrleydaniel18@gmail.com>
2026-02-17 21:44:01 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-
-int main() {
+int main(){
     /*========== CARTA 1 ==========*/
     char estado1;
     char codigo1[4];
@@ -10,6 +9,8 @@ int main() {
     float area1;
     float pib1;
     int pontos1;
+    float densidade1;
+    float perCapita1;
 
     /*========== CADASTRO DE CARTA 1 ==========*/
     printf("\nCADASTRO DE CARTA 1\n");
@@ -35,6 +36,10 @@ int main() {
     printf("A quantidade de pontos turísticos: \n");
     scanf("%d", &pontos1);
 
+    perCapita1 = pib1 / populacao1;
+    densidade1 = populacao1 / area1;
+
+
     /*========== CARTA 2 ==========*/
     char estado2;
     char codigo2[4];
@@ -43,6 +48,8 @@ int main() {
     float area2;
     float pib2;
     int pontos2;
+    float densidade2;
+    float perCapita2;
 
     /*========== CADASTRO DE CARTA 2 ==========*/
     printf("CADASTRO DE CARTA 2\n");
@@ -68,6 +75,9 @@ int main() {
     printf("A quantidade de pontos turísticos: \n");
     scanf("%d", &pontos2);
 
+    perCapita2 = pib2 / populacao2;
+    densidade2 = populacao2 / area2;
+
     /*========== CARTA 1 ==========*/
     printf("\nCarta 1:\n");
     printf("Estado: %c\n", estado1);
@@ -77,6 +87,8 @@ int main() {
     printf("Area: %.2f km²\n", area1);
     printf("PIB: %.2f bilhoes de reais\n", pib1);
     printf("Numero de Pontos Turisticos: %d\n", pontos1);
+    printf("Densidade populacional: %.2f hab/km²\n", densidade1);
+    printf("PIB per Capita: %.2f reais\n", perCapita1);
 
     /*========== CARTA 2 ==========*/
     printf("\nCarta 2:\n");
@@ -87,6 +99,8 @@ int main() {
     printf("Area: %.2f km²\n", area2);
     printf("PIB: %.2f bilhoes de reais\n", pib2);
     printf("Numero de Pontos Turisticos: %d\n", pontos2);
-    
+    printf("Densidade populacional: %.2f hab/km²\n", densidade2);
+    printf("PIB per Capita: %.2f reais\n", perCapita2);
+
     return 0;
 }
